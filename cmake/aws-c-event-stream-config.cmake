a4bc079de10d3d296c31fd36dd9884f3c06ff921
Marco Magdy <magmarco@amazon.com>
2018-10-10 15:16:33 -0700

    Do proper CMake packaging & installation
    This allows upstream projects to only do find_package() on
    aws-c-event-stream without having to know or worry about its dependencies,
    (i.e. aws-checksums, aws-c-common, etc.)

---
+++ cmake/aws-c-event-stream-config.cmake
@@ -0,0 +1,4 @@
+include(${CMAKE_CURRENT_LIST_DIR}/@CMAKE_PROJECT_NAME@-targets.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/aws-c-common-targets.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/aws-checksums-targets.cmake)
+
