e73e3b256230e89071584ba7a5b4ac739e5d4728
Marco Magdy <magmarco@amazon.com>
2018-10-15 15:23:52 -0700

    Disable visibility=hidden on functions marked as API

--- include/aws/event-stream/event_stream_exports.h
+++ include/aws/event-stream/event_stream_exports.h
@@ -14,18 +14,26 @@
 * express or implied. See the License for the specific language governing
 * permissions and limitations under the License.
 */
-#if defined (USE_WINDOWS_DLL_SEMANTICS) || defined (WIN32)
-#ifdef USE_IMPORT_EXPORT
-#ifdef AWS_EVENT_STREAM_EXPORTS
-#define AWS_EVENT_STREAM_API __declspec(dllexport)
-#else
-#define AWS_EVENT_STREAM_API __declspec(dllimport)
-#endif /* AWS_EVENT_STREAM_EXPORTS_H_ */
-#else
-#define AWS_EVENT_STREAM_API
-#endif // USE_IMPORT_EXPORT
-#else // defined (USE_WINDOWS_DLL_SEMANTICS) || defined (WIN32)
-#define AWS_EVENT_STREAM_API
-#endif // defined (USE_WINDOWS_DLL_SEMANTICS) || defined (WIN32)
+#if defined(USE_WINDOWS_DLL_SEMANTICS) || defined(WIN32)
+#    ifdef AWS_EVENT_STREAM_USE_IMPORT_EXPORT
+#        ifdef AWS_EVENT_STREAM_EXPORTS
+#            define AWS_EVENT_STREAM_API __declspec(dllexport)
+#        else
+#            define AWS_EVENT_STREAM_API __declspec(dllimport)
+#        endif /* AWS_EVENT_STREAM_EXPORTS */
+#    else
+#        define AWS_EVENT_STREAM_API
+#    endif /* AWS_EVENT_STREAM_USE_IMPORT_EXPORT */
 
-#endif /*AWS_EVENT_STREAM_EXPORTS_H_*/
+#else /* defined (USE_WINDOWS_DLL_SEMANTICS) || defined (WIN32) */
+
+#    if ((__GNUC__ >= 4) || defined(__clang__)) && defined(AWS_EVENT_STREAM_USE_IMPORT_EXPORT) && defined(AWS_EVENT_STREAM_EXPORTS)
+#        define AWS_EVENT_STREAM_API __attribute__((visibility("default")))
+#    else
+#        define AWS_EVENT_STREAM_API
+#    endif /* __GNUC__ >= 4 || defined(__clang__) */
+
+#endif /* defined (USE_WINDOWS_DLL_SEMANTICS) || defined (WIN32) */
+
+#endif /* AWS_EVENT_STREAM_EXPORTS_H */
+
