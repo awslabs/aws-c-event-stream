ab43bf99b71f2b3893ece66699d006ff35a9278c
Jonathan M. Henson <henso@amazon.com>
2019-09-13 17:37:47 -0700

    Added support for shared/static lib builds in the same build closure.

--- cmake/aws-c-event-stream-config.cmake
+++ cmake/aws-c-event-stream-config.cmake
@@ -2,5 +2,9 @@ include(CMakeFindDependencyMacro)
 find_dependency(aws-c-common)
 find_dependency(aws-checksums)
 
-include(${CMAKE_CURRENT_LIST_DIR}/@CMAKE_PROJECT_NAME@-targets.cmake)
+if (BUILD_SHARED_LIBS)
+    include(${CMAKE_CURRENT_LIST_DIR}/shared/@CMAKE_PROJECT_NAME@-targets.cmake)
+else()
+    include(${CMAKE_CURRENT_LIST_DIR}/static/@CMAKE_PROJECT_NAME@-targets.cmake)
+endif()
 
