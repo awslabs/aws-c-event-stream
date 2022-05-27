603a26dd151cacabe507fae8214063867422483f
Bret Ambrose <bretambrose@gmail.com>
2022-05-27 10:19:51 -0700

    More comments

--- include/aws/event-stream/event_stream_rpc_server.h
+++ include/aws/event-stream/event_stream_rpc_server.h
@@ -46,6 +46,12 @@ typedef void(aws_event_stream_rpc_server_connection_protocol_message_fn)(
 /**
  * Invoked when a new stream has been received on the connection. If you return AWS_OP_SUCCESS (0),
  * You must fill in the fields for continuation options or the program will assert and exit.
+ *
+ * A failure path MUST leave the ref count of the continuation alone.
+ *
+ * A success path should probably take a ref which will leave the continuation (assuming no other interference)
+ * at two AFTER creation is complete: 1 for the connection's continuation table, and one for the callback
+ * recipient which is presumably tracking it as well.
  */
 typedef int(aws_event_stream_rpc_server_on_incoming_stream_fn)(
     struct aws_event_stream_rpc_server_connection *connection,
